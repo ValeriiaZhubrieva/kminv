60363cf1fb7a92d958114407b468bf603ff20bf9
ValeriiaZhubrieva <67803539+ValeriiaZhubrieva@users.noreply.github.com>
2023-04-09 13:15:53 +0300

    start proj

---
+++ files/sendmail/sendmail.php
@@ -0,0 +1,71 @@
+<?php
+	use PHPMailer\PHPMailer\PHPMailer;
+	use PHPMailer\PHPMailer\Exception;
+
+	require 'phpmailer/src/Exception.php';
+	require 'phpmailer/src/PHPMailer.php';
+	require 'phpmailer/src/SMTP.php';
+
+	$name = trim($_POST['name']);
+ 	$email = trim($_POST['email']);
+ 	$message = trim($_POST['message']);
+
+	$mail = new PHPMailer(true);
+	$mail->CharSet = 'UTF-8';
+	$mail->setLanguage('ru', 'phpmailer/language/');
+	$mail->IsHTML(true);
+	
+	/*
+	$mail->isSMTP();                                            //Send using SMTP
+	$mail->Host       = 'smtp.example.com';                     //Set the SMTP server to send through
+	$mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+	$mail->Username   = 'user@example.com';                     //SMTP username
+	$mail->Password   = 'secret';                               //SMTP password
+	$mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+	$mail->Port       = 465;                 
+	*/
+
+	//Від кого лист
+	$mail->setFrom('sales@kminvestmentgroup.com'); // Вказати потрібний E-mail
+  //Кому відправити
+  	$mail->addAddress('sales@kminvestmentgroup.com'); // Вказати потрібний E-mail
+  //Тема листа
+  	$mail->Subject = 'Заявка с сайта kminvestmentgroup.com';
+
+  //Тіло листа
+  	$body = '<h1>Заявка с сайта kminvestmentgroup.com</h1>';
+  	$body .='<p><strong>Name:</strong>'.$name.'</p>';
+  	$body .='<p><strong>Email:</strong>'.$email.'</p>';
+  	$body.='<p><strong>Question</strong>'.$message.'</p>';
+	//if(trim(!empty($_POST['email']))){
+		//$body.=$_POST['email'];
+	//}	
+	
+	/*
+	//Прикріпити файл
+	if (!empty($_FILES['image']['tmp_name'])) {
+		//шлях завантаження файлу
+		$filePath = __DIR__ . "/files/sendmail/attachments/" . $_FILES['image']['name']; 
+		//грузимо файл
+		if (copy($_FILES['image']['tmp_name'], $filePath)){
+			$fileAttach = $filePath;
+			$body.='<p><strong>Фото у додатку</strong>';
+			$mail->addAttachment($fileAttach);
+		}
+	}
+	*/
+
+	$mail->Body = $body;
+
+	//Відправляємо
+	if (!$mail->send()) {
+		$message = 'Помилка';
+	} else {
+		$message = 'Дані надіслані!';
+	}
+
+	$response = ['message' => $message];
+
+	header('Content-type: application/json');
+	echo json_encode($response);
+?>
